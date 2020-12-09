0ec9cd7da0b6757a2790563880abe64a36be8eca
ericTang <erictang0212@gmail.com>
2020-12-09 13:09:36 +0800

    - create RandomMianName, RandomAdjName data structure

---
+++ Real/Real/Model/RandomMainName.swift
@@ -0,0 +1,15 @@
+//
+//  RandomMainName.swift
+//  Real
+//
+//  Created by 唐紹桓 on 2020/12/9.
+//
+
+import Foundation
+
+struct RandomMainName: Codable {
+    
+    let id: Int
+    
+    let name: String
+}
